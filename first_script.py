bafa5c80eb1d91cce758acd682244a4251853422
dswhit <dustwhitney@gmail.com>
2013-12-27 17:45:36 -0600

    \

---
+++ first_script.py
@@ -0,0 +1,18 @@
+#!/Users/dustin/bin
+
+#	first_script.py
+#	This script is intended to be a test for GitHub.
+#	It probably won't do much, but let's see what I come up with.
+
+from sys import argv
+
+
+input = argv[1]
+
+print "GitHub is a %s program" % input
+
+
+
+
+
+
